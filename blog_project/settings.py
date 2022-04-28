30ed6366adbd236574c65dada94ee77a2e8f9cf8
devendra-rgb <devendratumu@gmai..com>
2022-04-28 23:48:36 +0530

    modifid setting

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5%biuw*o*92l%b6&_1ey8thwgnuv$vk(4%ysd90zix8_(@^h66
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
