8d12f5375b4746a7de48bedbd428f6724126b532
devendra-rgb <devendratumu@gmai..com>
2022-04-28 23:52:04 +0530

    done

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'whitenoise.runserver_nonstatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
